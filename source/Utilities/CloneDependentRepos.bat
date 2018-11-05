a0af73bbe03afda627567e7b11d33a681d517b7b
SaiP <Sai.Phaneendra@cybertech.com>
2018-11-05 23:09:38 +0530

    Updating military symbology repository reference

--- source/Utilities/CloneDependentRepos.bat
+++ source/Utilities/CloneDependentRepos.bat
@@ -7,5 +7,5 @@ pwd
 REM Note: add "--single-branch" if you only want the single dev branch
 git clone -b dev https://github.com/Esri/coordinate-conversion-addin-dotnet
 git clone -b dev https://github.com/Esri/distance-direction-addin-dotnet.git
-git clone -b dev https://github.com/Esri/military-symbol-editor-addin-wpf
+git clone -b dev https://github.com/Esri/military-symbology.git
 git clone -b dev https://github.com/Esri/visibility-addin-dotnet
